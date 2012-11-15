39fcc6448853a4bed14de7f058d237fb7ccc9775
harsha <admin@yanasoftware.com>
2012-11-15 07:48:55 -0600

    third example second commit

--- ThirdProject/src/main/java/example2.java
+++ ThirdProject/src/main/java/example2.java
@@ -8,5 +8,7 @@
 public class example2 {
     public static void main(String[] args) {
         System.out.println("************Secondclass of third project*****************");
+        System.out.println("************Secondclass of third project*****************");
+        System.out.println("************Secondclass of third project*****************");
     }
 }

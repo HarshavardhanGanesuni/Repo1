23898189c0bcc96aa1c5ac8fe529012598417afd
harsha <admin@yanasoftware.com>
2012-11-15 07:55:08 -0600

    Revert "third example second commit"
    This reverts commit 39fcc6448853a4bed14de7f058d237fb7ccc9775.

--- ThirdProject/src/main/java/example2.java
+++ ThirdProject/src/main/java/example2.java
@@ -8,7 +8,5 @@
 public class example2 {
     public static void main(String[] args) {
         System.out.println("************Secondclass of third project*****************");
-        System.out.println("************Secondclass of third project*****************");
-        System.out.println("************Secondclass of third project*****************");
     }
 }

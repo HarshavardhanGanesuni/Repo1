593c201bd49c6709b1f7c2dfdeb796f368579bb4
harsha <admin@yanasoftware.com>
2012-11-15 06:36:12 -0600

    Second commit on firstexample

--- FirstExample/src/main/java/Test1.java
+++ FirstExample/src/main/java/Test1.java
@@ -7,5 +7,7 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("***Testing Class2*********");
+        System.out.println("***Testing Class2*********");
+        System.out.println("***Testing Class2*********");
     }
 }

7b6c5140a3dac2a51cd2afe5e6d71904b3678aaa
firstuser <admin@yanasoftware.com>
2012-11-23 11:58:53 +0530

    b

---
+++ SecondProject/src/main/java/Example.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: indianeagle
+ * Date: 11/15/12
+ * Time: 5:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("****Testing second project");
+        System.out.println("1 2 3 4 5 6 7 8 9");
+    }
+}

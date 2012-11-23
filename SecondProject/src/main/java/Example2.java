b96d205033423b3c5fbdc22cb422d1d1acfd2b24
firstuser <admin@yanasoftware.com>
2012-11-23 11:59:02 +0530

    b

---
+++ SecondProject/src/main/java/Example2.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: indianeagle
+ * Date: 11/15/12
+ * Time: 5:08 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Example2 {
+    public static void main(String[] args) {
+        System.out.println("*********Second class of second project***************");
+        System.out.println("123456789");
+    }
+}
